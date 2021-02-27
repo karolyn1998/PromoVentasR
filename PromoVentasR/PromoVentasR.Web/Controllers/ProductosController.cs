6a13ab0796041363b01e8f446b3c895a43da764f
KAROLYN FABIOLA VENTURA REDONDO <karolyn.ventura@unah.hn>
2021-02-27 17:11:54 -0600

    creando un controlador de productos

---
+++ PromoVentasR/PromoVentasR.Web/Controllers/ProductosController.cs
@@ -0,0 +1,23 @@
+﻿using PromoVentasR.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PromoVentasR.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto = new ProductoModel();
+            producto.Id = 1;
+            producto.Descripcion = "Saco de Maseca";
+
+
+            return View();
+        }
+    }
+}
