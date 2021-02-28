d0a7a9da3f230210abf1a82cc5e95ac04be9a088
Usuario <Usuario@DESKTOP-R6388TV>
2021-02-27 23:17:12 -0600

    Mantenimiento accion listar

---
+++ PromoVentasR/PromoVentas.WebAdmin/Controllers/ProductosController.cs
@@ -0,0 +1,27 @@
+﻿using PromoVentas.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PromoVentas.WebAdmin.Controllers
+{
+    public class ProductosController : Controller
+    {
+        ProductosBL _productosBL;
+
+        public ProductosController()
+        {
+            _productosBL = new ProductosBL();
+        }
+
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var listadeProductos = _productosBL.ObtenerProductos();
+
+            return View(listadeProductos);
+        }
+    }
+}
