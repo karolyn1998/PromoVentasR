630f7b37231b56f3bdc91210e619e39947e75203
Usuario <Usuario@DESKTOP-R6388TV>
2021-02-28 02:58:39 -0600

    categorias

---
+++ PromoVentasR/PromoVentas.BL/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace PromoVentas.BL
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
--- PromoVentasR/PromoVentas.BL/Producto.cs
+++ PromoVentasR/PromoVentas.BL/Producto.cs
@@ -8,9 +8,16 @@ namespace PromoVentas.BL
 {
     public class Producto
     {
+        public Producto()
+        {
+            Activo = true;
+        }
+
         public int Id { get; set; }
         public string  Descripcion { get; set; }
         public double Precio { get; set; }
+        public Categoria Categoria { get; set; }
+        public bool Activo { get; set; }
        
     }
 }
